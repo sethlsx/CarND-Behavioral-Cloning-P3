3c68aa5a7463783db3c1977d4febed43fbb0ea88
sethlsx <sethl@qq.com>
2018-10-24 17:17:49 +0800

    ready to train on aws

--- clone.py
+++ clone.py
@@ -5,6 +5,9 @@ import numpy as np
 import csv
 import cv2
 import matplotlib.image as mpimg
+import sklearn
+from sklearn.model_selection import train_test_split
+from random import shuffle
 
 lines = []
 
@@ -12,6 +15,43 @@ with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
+
+train_samples, validation_samples = train_test_split(lines, test_size = 0.2)
+
+#define a generator
+def generator(samples, batch_size = 32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i in range(3):
+                    name = './data/IMG/' + batch_sample[i].split('/')[-1]
+                    image = mpimg.imread(name)
+                    images.append(image)
+                    if i == 0:
+                        angle = float(batch_sample[3])
+                    else:
+                        angle = float(batch_sample[3]) + (-0.4)*i+0.6
+                    angles.append(angle)
+            #Flip the image to get more data
+
+            augmented_images, augmented_angles = [], []
+            for image, angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_angles.append(angle*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+'''
 images = []
 measurements = []
 for line in lines:
@@ -26,22 +66,22 @@ for line in lines:
         else:
             measurement = float(line[3]) + (-0.4)*i+0.6
         measurements.append(measurement)
-#Flip the image to get more data
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement*-1.0)
+
+
+
 
 X_train = np.array(images)
 y_train = np.array(measurements)
+'''
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
 
 #This is the model
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D
 from keras.layers.convolutional import Conv2D
 #from keras.layers.core import Activation
 from keras.layers.pooling import MaxPooling2D
@@ -49,11 +89,11 @@ from keras.layers.pooling import MaxPooling2D
 model = Sequential()
 
 #Cropping image
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
 
 
 #Lambda layer
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(lambda x: (x / 127.5) - 1.))
 
 #Layer 1
 model.add(Conv2D(3, kernel_size = (5, 5), padding = 'valid', activation = 'relu'))
@@ -64,7 +104,7 @@ model.add(Conv2D(24, kernel_size = (5, 5), padding = 'valid', activation = 'relu
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 #Layer 3
-model.add(Conv2D(36, kernel_size = (5, 5), padding = 'valid', activation = 'relu'))
+model.add(Conv2D(36, kernel_size = (3, 3), padding = 'valid', activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
@@ -73,7 +113,7 @@ model.add(Conv2D(48, kernel_size = (3, 3), padding = 'valid', activation = 'relu
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 #Layer 5
-model.add(Conv2D(64, kernel_size = (3, 3), padding = 'valid', activation = 'relu'))
+model.add(Conv2D(64, kernel_size = (1, 1), padding = 'valid', activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(rate = 0.6))
 
@@ -111,8 +151,8 @@ model.compile(loss = 'mse', optimizer = 'adam')
 from keras.models import Model
 import matplotlib.pyplot as plt
 
-history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples), \
-    validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch = 5, verbose = 1)
+history_object = model.fit_generator(train_generator, steps_per_epoch = len(train_samples), \
+    validation_data = validation_generator, validation_steps = len(validation_samples), epochs = 1, verbose = 1)
 
 print(history_object.history.keys())
 
@@ -124,7 +164,7 @@ plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show
+plt.show()
 #Save the model
 
 model.save('model.h5')
